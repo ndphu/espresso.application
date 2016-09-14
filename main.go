223e357863713051c0782adacecf886279691a92
ndphu <ngdacphu.khtn@gmail.com>
2016-09-15 01:22:49 +0700

    use StopRequestCallback to stop the application actively

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 
 type Application interface {
 	Startup()
-	GetSignalChannel() chan os.Signal
+	SetStopRequestCallback(stopCallback Callback)
 	Run()
 	Shutdown()
 }
 
+type Callback func()
+
 func RunApplication(a Application) {
 	a.Startup()
 	go a.Run()
-	sigchan := a.GetSignalChannel()
+	//sigchan := a.GetSignalChannel()
+	sigchan := make(chan os.Signal, 1)
+	a.SetStopRequestCallback(func() {
+		close(sigchan)
+	})
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt, os.Kill)
 	for {
 		sig, success := <-sigchan
--- main_test.go
+++ main_test.go
@@ -3,16 +3,17 @@ package application_test
 import (
 	application "github.com/ndphu/espresso.application"
 	"github.com/stretchr/testify/assert"
-	"os"
+	//"os"
 	"testing"
 	"time"
 )
 
 type ApplicationTest struct {
-	SignalChannel  chan os.Signal
-	StartupCalled  bool
-	RunCalled      bool
-	ShutdownCalled bool
+	//SignalChannel  chan os.Signal
+	StopRequestCallback application.Callback
+	StartupCalled       bool
+	RunCalled           bool
+	ShutdownCalled      bool
 }
 
 func (a *ApplicationTest) Startup() {
@@ -27,17 +28,22 @@ func (a *ApplicationTest) Shutdown() {
 	a.ShutdownCalled = true
 }
 
-func (a *ApplicationTest) GetSignalChannel() chan os.Signal {
-	return a.SignalChannel
+// func (a *ApplicationTest) GetSignalChannel() chan os.Signal {
+// 	return a.SignalChannel
+// }
+
+func (a *ApplicationTest) SetStopRequestCallback(callback application.Callback) {
+	a.StopRequestCallback = callback
 }
 
 func TestApplication(t *testing.T) {
 	testApp := new(ApplicationTest)
-	testApp.SignalChannel = make(chan os.Signal, 1)
+	//testApp.SignalChannel = make(chan os.Signal, 1)
 
 	go func() {
 		time.Sleep(200 * time.Millisecond)
-		close(testApp.GetSignalChannel())
+		//close(testApp.GetSignalChannel())
+		testApp.StopRequestCallback()
 	}()
 
 	application.RunApplication(testApp)
